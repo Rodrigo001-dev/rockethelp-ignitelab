10ebbd617acd364a1c6483d3b79e42d004e29690
Rodrigo <rodrigorael53@gmail.com>
2022-08-05 00:52:18 -0300

    making treatment so that when the fonts are not loaded a loading screen appears

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
-import { NativeBaseProvider } from 'native-base';
+import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { THEME } from './src/styles/theme';
+
 import { SignIn } from './src/screens/SignIn';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
   // o fontsLoaded vai armazenar um valor boolean se a fonte carregou ou não
@@ -11,7 +13,18 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <SignIn />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {
+        /* 
+          se as fontes já carregaram(fontsLoaded = true) vai mostrar o SignIn
+          Se não vai mostrar o componente de Loading
+        */
+      }
+      { fontsLoaded ? <SignIn /> : <Loading /> }
     </NativeBaseProvider>
   );
 };
