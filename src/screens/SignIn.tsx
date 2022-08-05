8b519052621e4cbb08037d8ee4c3cbb9f2f932b6
Rodrigo <rodrigorael53@gmail.com>
2022-08-05 02:15:19 -0300

    adding the state for the Inputs

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Heading, VStack, Icon, useTheme } from 'native-base';
 import { Envelope, Key } from 'phosphor-react-native';
 
@@ -7,6 +8,9 @@ import { Input } from '../components/Input';
 import { Button } from '../components/Button';
 
 export function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const { colors } = useTheme();
 
   return (
@@ -21,12 +25,14 @@ export function SignIn() {
         mb={4}
         placeholder="E-mail"
         InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
+        onChangeText={setEmail}
       />
       <Input
         mb={8}
         placeholder="Senha"
         InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
         secureTextEntry
+        onChangeText={setPassword}
       />
 
       <Button title="Entrar" w="full" />
