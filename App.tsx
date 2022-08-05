90510d442a3a3b1aa0fd43793108e872781d4b01
Rodrigo <rodrigorael53@gmail.com>
2022-08-05 00:26:22 -0300

    overrrinding native-base default theme for my theme

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import { NativeBaseProvider } from 'native-base';
+
+import { THEME } from './src/styles/theme';
 import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <SignIn />
     </NativeBaseProvider>
   );
