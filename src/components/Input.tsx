ceb61c85d564c4e53cb2b5d1b165211bc72f6425
Rodrigo <rodrigorael53@gmail.com>
2022-08-05 01:47:04 -0300

    finishin the Input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -11,6 +11,11 @@ export function Input({ ...rest }: IInputProps) {
       fontFamily="body"
       color="white"
       placeholderTextColor="gray.300"
+      _focus={{
+        borderWidth: 1,
+        borderColor: "green.500",
+        bg: "gray.700"
+      }}
       {...rest}
     />
   );
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,10 +1,13 @@
-import { Heading, VStack } from 'native-base';
+import { Heading, VStack, Icon, useTheme } from 'native-base';
+import { Envelope, Key } from 'phosphor-react-native';
 
 import Logo from '../assets/logo_primary.svg';
 
 import { Input } from '../components/Input';
 
 export function SignIn() {
+  const { colors } = useTheme();
+
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
       <Logo />
@@ -15,10 +18,13 @@ export function SignIn() {
 
       <Input
         placeholder="E-mail"
-        
+        mb={4}
+        InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
       />
       <Input
         placeholder="Senha"
+        InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+        secureTextEntry
       />
 
     </VStack>
