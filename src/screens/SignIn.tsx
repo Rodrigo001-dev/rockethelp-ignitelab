e8e1e2b8f99cbbf5dd88df6d176606cd542e6ff8
Rodrigo <rodrigorael53@gmail.com>
2022-08-05 01:22:35 -0300

    doing the configs to my app understard svg

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,9 +1,15 @@
-import { VStack } from 'native-base';
+import { Heading, VStack } from 'native-base';
+
+import Logo from '../assets/logo_primary.svg';
 
 export function SignIn() {
   return (
-    <VStack>
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
       
+      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+        Acesse a sua conta
+      </Heading>
+
     </VStack>
   );
 };
