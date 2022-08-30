9913230e283b0e4ad678a6aa57aa485b48a5adc9
Rodrigo <rodrigorael53@gmail.com>
2022-08-30 16:10:09 -0300

    adding a plugin in the app

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -62,10 +62,10 @@ export function Home() {
       <VStack flex={1} px={6}>
         <HStack w="full" mt={8} mb={4} justifyContent="space-between" alignItems="center">
           <Heading color="gray.100">
-            Meus chamados
+            Solicitações
           </Heading>
           <Text color="gray.200">
-            3
+            {orders.length}
           </Text>
         </HStack>
 
