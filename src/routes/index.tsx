84c8bdcea70011ce643dfbee393585cc036a574d
Rodrigo <rodrigorael53@gmail.com>
2022-08-30 17:39:29 -0300

    performing a check so that if the user is logged in redirect him to the orders screen if not send him to the signIn screen

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,43 @@
+import { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
 
+import { Loading } from '../components/Loading';
 import { SignIn } from '../screens/SignIn';
+
 import { AppRoutes } from './app.routes';
 
 export function Routes() {
+  const [loading, setIsLoading] = useState(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User>();
+
+  useEffect(() => {
+    // no subscriber eu consigo ficar observando se o usuário está ou não 
+    // autenticado atraves do método auth().onAuthStateChanged()
+    const subscriber = auth()
+    .onAuthStateChanged(response  => {
+      // pegando o resonse(que vai falar se o usuário está ou não autenticado)
+      // e inserindo essa informação no estado user
+      setUser(response);
+      setIsLoading(false);
+    });
+
+    return subscriber;
+  }, []);
+
+  if(loading) {
+    return <Loading />
+  };
+
   return (
     <NavigationContainer>
-      <AppRoutes />
+      {
+        /* 
+          se o usuário existir e estiver logado vai ser exibido o AppRoutes senão
+          vai ser exibido o SignIn
+        */
+      }
+      {user ? <AppRoutes /> : <SignIn />}
     </NavigationContainer>
   )
 };
