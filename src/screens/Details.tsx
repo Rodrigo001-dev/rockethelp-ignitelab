052e56c5fc7e5169418a3118254405a69eb3a21b
Rodrigo <rodrigorael53@gmail.com>
2022-09-08 09:50:54 -0300

    adding the CardDetails component in the Details screen and create a function to update the data into firestore

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,15 +1,19 @@
 import { useEffect, useState } from 'react';
-import { HStack, Text, VStack, useTheme, ScrollView } from 'native-base';
-import { useRoute } from '@react-navigation/native';
+import { Alert } from 'react-native';
+import { HStack, Text, VStack, Box, useTheme, ScrollView } from 'native-base';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import firestore from '@react-native-firebase/firestore';
 import { OrderFirestoreDTO } from '../DTOs/OrderFirestoreDTO';
-import { CircleWavyCheck, Hourglass } from 'phosphor-react-native';
+import { CircleWavyCheck, Hourglass, DesktopTower, Clipboard } from 'phosphor-react-native';
 
 import { dateFormat } from '../utils/firestoreDateFormat';
 
 import { Header } from '../components/Header';
 import { OrderProps } from '../components/Order';
 import { Loading } from '../components/Loading';
+import { Button } from '../components/Button';
+import { Input } from '../components/Input';
+import { CardDetails } from '../components/CardDetails';
 
 type RouteParams = {
   orderId: string;
@@ -26,9 +30,35 @@ export function Details() {
   const [isLoading, setIsLoading] = useState(true);
   const [order, setOrder] = useState<OrderDetails>({} as OrderDetails);
 
+  const navigation = useNavigation();
   const { colors } = useTheme();
   const route = useRoute();
+
   const { orderId } = route.params as RouteParams;
+
+  function handleOrderClose() {
+    if (!solution) {
+      return Alert.alert('Solicitação', 'Informe a solução para encerrar a solicitação');
+    };
+
+    firestore()
+    .collection<OrderFirestoreDTO>('orders')
+    .doc(orderId)
+    .update({
+      // alterando esses campos
+      status: 'closed',
+      solution,
+      closed_at: firestore.FieldValue.serverTimestamp()
+    })
+    .then(() => {
+      Alert.alert('Solicitação', 'Solicitação encerrada.');
+      navigation.goBack();
+    })
+    .catch((error) => {
+      console.log(error);
+      Alert.alert('Solicitação', 'Não foi possível encerrar a solicitação');
+    })
+  };
 
   useEffect(() => {
     firestore()
@@ -71,7 +101,9 @@ export function Details() {
 
   return (
     <VStack flex={1} bg="gray.700">
-      <Header title="solicitação" />
+      <Box px={6} bg="gray.600">
+        <Header title="solicitação" />
+      </Box>
       
       <HStack bg="gray.500" justifyContent="center" p={4}>
         {
@@ -91,8 +123,49 @@ export function Details() {
       </HStack>
 
       <ScrollView mx={5} showsVerticalScrollIndicator={false}>
+        <CardDetails 
+          title="equipamento"
+          description={`Patrimônio ${order.patrimony}`}
+          icon={DesktopTower}
+          footer={order.when}
+        />
 
+        <CardDetails 
+          title="descrição do problema"
+          description={order.description}
+          icon={DesktopTower}
+        />
+
+        <CardDetails 
+          title="solução"
+          description={order.solution}
+          icon={CircleWavyCheck}
+          footer={order.closed && `Encerrado em ${order.closed}`}
+        >
+          {
+            order.status === 'open' &&
+            <Input
+              placeholder="Descrição da solução"
+              onChangeText={setSolution}
+              textAlignVertical="top"
+              multiline
+              h={24}
+            />
+          }
+        </CardDetails>
       </ScrollView>
+
+      {
+        // se estiver aberta(order.status === 'open') vai aparecer um Button
+        // para poder fechar a solicitação
+        order.status === 'open' &&
+        <Button 
+          title="Encerrar solicitação"
+          m={5}
+          onPress={handleOrderClose}
+        />
+      }
+
     </VStack>
   );
 }
